7fb791f2915076627660ebdb14c65214132082e0
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-07-14 02:23:10 +0600

    Implement themeprovider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="h1-bold">This is a piece of text</h1>
       <UserButton />
     </div>
   );
